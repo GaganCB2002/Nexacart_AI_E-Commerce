9369aaf5379bded5018b91c0e03504204193d9dd
Gagan C B <gagancb2002@gmail.com>
2026-01-05 11:36:16 +0530

    Delete src/main directory

--- src/main/java/com/ecommerce/PriceHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
-    List<PriceHistory> findByProductIdOrderByTimestampAsc(Long productId);
-}
--- src/main/java/com/ecommerce/RecentViewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RecentViewRepository extends JpaRepository<RecentView, Long> {
-    List<RecentView> findByUserIdOrderByViewedAtDesc(Long userId);
-}
